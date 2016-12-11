2b2a343ce9b9120c196332860801f0697b2dee2b
Stefan Reichel <dev@bomret.com>
2016-12-11 14:39:54 +0100

    Updated tests

--- test/http.tests.ts
+++ test/http.tests.ts
@@ -1,89 +1,87 @@
 import * as request from "supertest";
-import "mocha";
-
 import { asRequestListener } from "../src";
-import { Ok, ifPath, parsePath } from "../src/http";
+import { ifPath, Ok, parsePath } from "../src/http";
 
 describe("When using ifPath", () => {
-  describe("on a matching path", () => {
-    const pipe = ifPath("/some/route", () => Ok());
+    describe("on a matching path", () => {
+        const pipe = ifPath("/some/route", () => Ok());
 
-    it("should execute the next pipe.", done => {
-      request(asRequestListener(pipe))
-        .get("/some/route")
-        .expect(200, done);
+        it("should execute the next pipe.", done => {
+            request(asRequestListener(pipe))
+                .get("/some/route")
+                .expect(200, done);
+        });
     });
-  });
 
-  describe("on a non-matching path", () => {
-    const pipe = ifPath("/some/route", () => Ok());
+    describe("on a non-matching path", () => {
+        const pipe = ifPath("/some/route", () => Ok());
 
-    it("should result in a 404.", done => {
-      request(asRequestListener(pipe))
-        .get("/some")
-        .expect(404, done);
+        it("should result in a 404.", done => {
+            request(asRequestListener(pipe))
+                .get("/some")
+                .expect(404, done);
+        });
     });
-  });
 });
 
 describe("When using parsePath", () => {
-  describe("on a url path with placeholders", () => {
-    interface Params {
-      foo: string;
-      bar: string;
-    }
+    describe("on a url path with placeholders", () => {
+        interface Params {
+            foo: string;
+            bar: string;
+        }
 
-    const pipe = parsePath<Params>("/route/:foo/some/:bar", params => Ok(JSON.stringify(params)));
+        const pipe = parsePath<Params>("/route/:foo/some/:bar", params => Ok(JSON.stringify(params)));
 
-    it("should parse the placeholder values correctly.", done => {
-      request(asRequestListener(pipe))
-        .get("/route/first/some/beer")
-        .expect(200, JSON.stringify({ foo: "first", bar: "beer" }), done);
+        it("should parse the placeholder values correctly.", done => {
+            request(asRequestListener(pipe))
+                .get("/route/first/some/beer")
+                .expect(200, JSON.stringify({ foo: "first", bar: "beer" }), done);
+        });
     });
-  });
 
-  describe("on a url path with one placeholder", () => {
-    interface Params {
-      foo: string;
-      bar: string;
-    }
+    describe("on a url path with one placeholder", () => {
+        interface Params {
+            foo: string;
+            bar: string;
+        }
 
-    const pipe = parsePath<Params>("/route/:foo/some", params => Ok(JSON.stringify(params)));
+        const pipe = parsePath<Params>("/route/:foo/some", params => Ok(JSON.stringify(params)));
 
-    it("should parse the placeholder value correctly.", done => {
-      request(asRequestListener(pipe))
-        .get("/route/first/some")
-        .expect(200, JSON.stringify({ foo: "first" }), done);
+        it("should parse the placeholder value correctly.", done => {
+            request(asRequestListener(pipe))
+                .get("/route/first/some")
+                .expect(200, JSON.stringify({ foo: "first" }), done);
+        });
     });
-  });
 
-  describe("on a url path with no placeholders", () => {
-    interface Params {
-      foo: string;
-      bar: string;
-    }
+    describe("on a url path with no placeholders", () => {
+        interface Params {
+            foo: string;
+            bar: string;
+        }
 
-    const pipe = parsePath<Params>("/route/some", params => Ok(JSON.stringify(params)));
+        const pipe = parsePath<Params>("/route/some", params => Ok(JSON.stringify(params)));
 
-    it("should pass on an empty object.", done => {
-      request(asRequestListener(pipe))
-        .get("/route/some")
-        .expect(200, "{}", done);
+        it("should pass on an empty object.", done => {
+            request(asRequestListener(pipe))
+                .get("/route/some")
+                .expect(200, "{}", done);
+        });
     });
-  });
 
-  describe("on a non-matching url path", () => {
-    interface Params {
-      foo: string;
-      bar: string;
-    }
+    describe("on a non-matching url path", () => {
+        interface Params {
+            foo: string;
+            bar: string;
+        }
 
-    const pipe = parsePath<Params>("/route/:foo/some", params => Ok(JSON.stringify(params)));
+        const pipe = parsePath<Params>("/route/:foo/some", params => Ok(JSON.stringify(params)));
 
-    it("should result in a 404.", done => {
-      request(asRequestListener(pipe))
-        .get("/route/some")
-        .expect(404, done);
+        it("should result in a 404.", done => {
+            request(asRequestListener(pipe))
+                .get("/route/some")
+                .expect(404, done);
+        });
     });
-  });
 });
